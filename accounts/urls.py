9fe268200a1dace995482f1b79de04f9f99e1816
Chiranjeeb Mahanta <chiranjeebm2410@gmail.com>
2019-08-17 11:24:35 +0530

    bootstrapped login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth.views import LoginView, LogoutView
+from accounts.forms import LoginForm
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('login/', LoginView.as_view(authentication_form=LoginForm,template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(template_name='templates/home.html'), name="logout"),
 ]
