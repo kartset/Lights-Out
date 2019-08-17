77d36986274f166d02469bc08fdd7491b2733203
Chiranjeeb Mahanta <chiranjeebm2410@gmail.com>
2019-08-17 23:33:26 +0530

    login and signup fixed

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(AuthenticationForm):
   username=forms.CharField(widget=forms.TextInput(
@@ -8,3 +10,31 @@ class LoginForm(AuthenticationForm):
   password=forms.CharField(widget=forms.PasswordInput(
   attrs={'class':'form-control', 'placeholder': 'Enter Password'}
   ), label="Password")
+
+class RegistrationForm(UserCreationForm):
+  email=forms.EmailField(required=True)
+
+  class Meta:
+    model=User
+    fields=(
+      'username',
+      'first_name',
+      'last_name',
+      'email',
+      'password1',
+      'password2'
+    )
+
+  def save(self, commit=True):
+    user=super(RegistrationForm, self).save(commit=False)
+    user.first_name=cleaned_data['first_name']
+    user.last_name=cleaned_data['last_name']
+    user.email=cleaned_data['email']
+
+    if commit:
+      user.save()
+    return user
+
+
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import RegistrationForm
+from accounts.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
 
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegistrationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def register(request):
+  if request.method =='POST':
+    form=RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return(redirect('/accounts'))
+  else:
+    form=RegistrationForm()
+    args={'form': form}
+    return render(request, 'templates/signup.html', args)
